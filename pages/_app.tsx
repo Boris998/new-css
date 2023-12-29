0c7078a8c49762a3e31151841174065b8ed0b2f9
boris <btadirov16@gmail.com>
2023-12-29 12:26:31 +0100

    transfer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
+import '@/pages/subgrid/subgrid.css'
+import '@/pages/layers/layers.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+
